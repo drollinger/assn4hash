21cb8038105c5b7ba824e98eb5ae2261da76b9c4
drollinger <dallin@blackstar>
2019-03-04 17:33:55 -0700

    started and a few changes to doublehash but nothing serious

---
+++ src/GameStarter.java
@@ -0,0 +1,53 @@
+/*
+import java.io.*;
+public class Game {
+    public Game(){
+        H = new QuadraticProbingHashTable<WordInfo>();
+    } ;
+
+    public int computeScore(WordInfo wi) {
+
+    }
+
+    public void playGame(String filename){
+
+    } ;
+
+    public String toString() {
+        int LIMIT = 20;
+        return name+ "\n"+ H.toString(LIMIT);
+    }
+
+
+    private String name;
+    private QuadraticProbingHashTable<WordInfo> H;
+
+
+
+    public static void main( String [ ] args ) {
+        try {
+            Game g0 = new Game(  );
+            g0.playGame("game0.txt" );
+            System.out.println(g0);
+/*            Game g1 = new Game(  );
+            g1.playGame("game1.txt" );
+            System.out.println(g1);
+            Game g2 = new Game(  );
+            g2.playGame("game2.txt" );
+            System.out.println(g2);
+            Game g3 = new Game(  );
+            g3.playGame("game3.txt" );
+            System.out.println(g3);
+            Game g4 = new Game(  );
+            g4.playGame("game4.txt" );
+            System.out.println(g4);/
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
+*/
